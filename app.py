8eb0b8bd5e7b8a7bc96d32cdc0df8c64d869587a
Alex777Russia <shevalex777@mail.ru>
2024-04-07 12:40:30 +0300

    deepface library added + classes added

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ import os
 import json
 import hashlib
 
-from model import find_photo, analysis_photo, compare_photos, add_photo
+from model import Model
 
 
 ###
 #       Configuration
 ###
 
+LOCAL_PATH = "./photos/"
+FIREBASE_PATH = "gallery/"
+
+model = Model(local_path=LOCAL_PATH, firebase_path=FIREBASE_PATH)
+
 telegram_token = json.load(open('telegram.json', 'rb'))
 bot = telebot.TeleBot(telegram_token["TOKEN"])
-LOCAL_PATH = "./photos/"
 
 waiting_for = {}
 
@@ -76,6 +80,7 @@ def handle_photo(message):
         elif waiting_for[chat_id]['need'] == 2 and waiting_for[chat_id]['count'] == 1:
             waiting_for[chat_id]['photo1'] = absolute_path
             bot.reply_to(message, "Отлично, я получил первую фотографию! Отправляйте вторую")
+            return
         
         elif waiting_for[chat_id]['need'] == 2 and waiting_for[chat_id]['count'] == 0:
             absolute_path_photo_1 = waiting_for[chat_id]['photo1']
@@ -124,34 +129,34 @@ def handle_photo(message):
 @bot.message_handler(commands=["Информация"])
 def info_btn(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id=chat_id, text='Этот бот сделан командой GoodAi:\n Петросов, Четверина, Уткин, Шемет и Шевченко\n Мои возможности:\n   1) Сравнение людей на двух изображений (необходимо загрузить два изображения) и бот ответит, один ли человек изображён на фотографиях.\n   2) Поиск человека по базе данных изображения (необходимо загрузить одно изображение) и бот ответит тремя двумя самыми похожими фотографиями.\n   3) Анализ фотографии на пол, рассовую принадлежность, возраст и эмоцю (необходимо отправить 1 фото)\n   4) Добавить фотографию в общую базу данных всех фотографий!\n Для выбора опций нажмите на соотвествующую кнопку далее')
+    bot.send_message(chat_id=chat_id, text='Этот бот сделан командой GoodAi:\nПетросов, Четверина, Уткин, Шемет и Шевченко\n\nМои возможности:\n   1) Сравнение людей на двух изображений\n(необходимо загрузить два изображения) и бот ответит, один ли человек изображён на фотографиях.\n   2) Поиск человека по базе данных изображения\n(необходимо загрузить одно изображение) и бот ответит тремя двумя самыми похожими фотографиями.\n   3) Анализ фотографии на пол, рассовую принадлежность, возраст и эмоцю\n(необходимо отправить 1 фото)\n   4) Добавить фотографию в общую базу данных всех фотографий!\nДля выбора опций нажмите на далее соотвествующую команду')
 
 
 @bot.message_handler(commands=["Сравнение"])
 def compare_btn(message):
     chat_id = message.chat.id
-    waiting_for[chat_id] = {'need': 2, 'count': 2, 'function': compare_photos}
+    waiting_for[chat_id] = {'need': 2, 'count': 2, 'function': model.compare_photos}
     bot.send_message(chat_id=chat_id, text='Отлично, вы выбрали сравнение людей на 2-х фотографиях! Теперь отправьте две фотографии')  
 
 
 @bot.message_handler(commands=['Анализ'])
 def analysis_btn(message):
     chat_id = message.chat.id
-    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': analysis_photo}
+    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': model.analysis_photo}
     bot.send_message(chat_id=chat_id, text='Отлично, вы выбрали анализ признаков человека! Теперь отправьте фотографию')  
 
 
 @bot.message_handler(commands=['Поиск'])
 def find_btn(message):
     chat_id = message.chat.id
-    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': find_photo}
+    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': model.find_photo}
     bot.send_message(chat_id=chat_id, text='Отлично, вы выбрали поиск человека! Теперь отправьте фотографию')
 
 
 @bot.message_handler(commands=['Добавление'])
 def add_btn(message):
     chat_id = message.chat.id
-    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': add_photo}
+    waiting_for[chat_id] = {'need': 1, 'count': 1, 'function': model.add_photo}
     bot.send_message(chat_id=chat_id, text='Отлично, вы выбрали поиск человека! Теперь отправьте фотографию')
 
 
--- model.py
+++ model.py
@@ -3,223 +3,218 @@ import numpy as np
 import pandas as pd
 import datetime
 
-from service import post_photo, get_photo
-
-backends = [
-  'opencv', 
-  'ssd', 
-  'dlib', 
-  'mtcnn', 
-  'retinaface', 
-  'mediapipe',
-  'yolov8',
-  'yunet',
-  'fastmtcnn',
-]
-
-models = [
-  "VGG-Face", 
-  "Facenet", 
-  "Facenet512", 
-  "OpenFace", 
-  "DeepFace", 
-  "DeepID", 
-  "ArcFace", 
-  "Dlib", 
-  "SFace",
-]
-
-metrics = ["cosine", "euclidean", "euclidean_l2"]
-
-MODEL_NAME: str = "VGG-Face"
-BACKEND: str = "yolov8"
-METRICS: str = "cosine"
-IMG_PATH: str = "gallery/1.jpg"
-START_ID: int = -1
-
-LOCAL_PATH_GALLERY: str = "photos/"
+from service import Service
 
 
-def prepare_data(data_base):
-    database_images_start = pd.read_csv(data_base)
-    database_images_start = database_images_start.drop('Unnamed: 0', axis=1)
+class Model:
+    __instance = None
 
-    result = database_images_start['vector'].apply(lambda x: np.array(list(map(float, (x[1:-2].split(", "))))))
-    database_images_start.vector = result
-    return database_images_start
+    __service = None
+    __db = None
 
-database_images = prepare_data("database.csv")
+    __MODEL_NAME: str = "VGG-Face"
+    __BACKEND: str = "yolov8"
+    __METRICS: str = "cosine"
 
-def cos(a, b):
-    return a@b/((a**2).sum() * (b**2).sum())
-
-def find_photo(path: str) -> tuple:
-    '''
-    Метод поиска двух фотографий, наиболее похожих на отправленную пользователем.
-    input:
-        path: str - путь локального хранения фотографии, отправленной пользователем
-    output:
-        (type: str, data: [str, str] or None, status: str)
-        data - список локальных путей к двум фотографиям скаченным фотографиям.
-    '''
-
-    data = None
-
-    try:
-        embedding_objs = represent(model_name=MODEL_NAME, 
-                      detector_backend=BACKEND, 
-                      img_path=path)
-        embedding = embedding_objs[0]["embedding"]
-        embedding = np.array(embedding)
-    except Exception as e:
-        print("Получение эмбединга поиска")
-        return ('FIND', data, 'ERR')
+    __backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe','yolov8','yunet','fastmtcnn']
+    __models = [ "VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
+    __metrics = ["cosine", "euclidean", "euclidean_l2"]
     
-    result = []
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+ 
+        return cls.__instance
+  
+    def __init__(self, local_path="./photos/", firebase_path="gallery/"):
+        self.__LOCAL_PATH = local_path
+        self.__REMOTE_PATH = firebase_path
 
-    try:
-        for i in range(database_images.shape[0]):
-            row = database_images.iloc[i]
-            result.append([cos(embedding, row.vector), row.url])
+        self.__service = Service(path_storage_config="firebase_storage_config.json",
+                                 local_path=self.__LOCAL_PATH,
+                                 firebase_path=self.__REMOTE_PATH)
 
-        result = sorted(result, reverse=False)
-        data = [result[-1], result[-2]]
-    except Exception as e:
-        print("Поиск лучших двух фотографий")
-        return ('FIND', data, 'ERR')
+        self.__db = self.__prepare_data("database.csv")
+    
 
-    try:
-        result_photo_1 = get_photo(save_local_path=LOCAL_PATH_GALLERY + data[0][1], from_url=data[0][1]) 
-        result_photo_2 = get_photo(save_local_path=LOCAL_PATH_GALLERY + data[1][1], from_url=data[1][1])
+    def __prepare_data(self, path_data_base="database.csv"):
+        db_images_start = pd.read_csv(path_data_base)
+        db_images_start = db_images_start.drop('Unnamed: 0', axis=1)
 
-        if (result_photo_1 == None) or (result_photo_2 == None):
+        result = db_images_start['vector'].apply(lambda x: np.array(list(map(float, (x[1:-2].split(", "))))))
+        db_images_start.vector = result
+        return db_images_start
+
+    @staticmethod
+    def cos(a, b):
+        return a@b/((a**2).sum() * (b**2).sum())
+
+    def find_photo(self, path: str) -> tuple:
+        '''
+        Метод поиска двух фотографий, наиболее похожих на отправленную пользователем.
+        input:
+            path: str - путь локального хранения фотографии, отправленной пользователем
+        output:
+            (type: str, data: [str, str] or None, status: str)
+            data - список локальных путей к двум фотографиям скаченным фотографиям.
+        '''
+
+        data = None
+
+        try:
+            embedding_objs = represent(model_name=self.__MODEL_NAME, 
+                      detector_backend=self.__BACKEND, 
+                      img_path=path)
+            embedding = embedding_objs[0]["embedding"]
+            embedding = np.array(embedding)
+        except Exception as e:
+            print("Получение эмбединга поиска")
+            return ('FIND', data, 'ERR')
+    
+        result = []
+
+        try:
+            for i in range(self.__db.shape[0]):
+                row = self.__db.iloc[i]
+                result.append([self.cos(embedding, row.vector), row.url])
+
+            result = sorted(result, reverse=False)
+            data = [result[-1], result[-2]]
+        except Exception as e:
+            print("Поиск лучших двух фотографий")
+            return ('FIND', data, 'ERR')
+
+        try:
+            result_photo_1 = self.__service.get_photo(to_local_path=self.__LOCAL_PATH + data[0][1], from_url=data[0][1]) 
+            result_photo_2 = self.__service.get_photo(to_local_path=self.__LOCAL_PATH + data[1][1], from_url=data[1][1])
+
+            if (result_photo_1 == None) or (result_photo_2 == None):
+                return ('FIND', None, 'ERR')
+        except Exception as e:
+            print("Ошибка запроса GET")
             return ('FIND', None, 'ERR')
-    except Exception as e:
-        print("Ошибка запроса GET")
-        return ('FIND', None, 'ERR')
 
-    return ('FIND', data, 'OK')
+        return ('FIND', data, 'OK')
 
-def analysis_photo(path: str) -> tuple:
-    '''
-    Метод анализа фотографии, возвращает возраст, пол, национальность и эмоцию.
-    input:
-        path: str - путь локального хранения фотографии, отправленной пользователем
-    output:
-        (type: str, data: str or None, status: str)
-        data - строка с признаками в необходимом формате, которую получит пользователь от бота.
-    '''
-    try:
-        data = analyze(img_path=path, actions=['age', 'gender', 'race', 'emotion'])
-    except Exception as e:
-        print("Работа метода анализа")
-        return ('ANLS', None, 'ERR')
+    def analysis_photo(self, path: str) -> tuple:
+        '''
+        Метод анализа фотографии, возвращает возраст, пол, национальность и эмоцию.
+        input:
+            path: str - путь локального хранения фотографии, отправленной пользователем
+        output:
+            (type: str, data: str or None, status: str)
+            data - строка с признаками в необходимом формате, которую получит пользователь от бота.
+        '''
+        try:
+            data = analyze(img_path=path, actions=['age', 'gender', 'race', 'emotion'])
+        except Exception as e:
+            print("Работа метода анализа")
+            return ('ANLS', None, 'ERR')
     
-    try:
-        data = data[0]
-        age = data['age']
-        gender = data['dominant_gender']
-        race = data['dominant_race']
-        emotion = data['dominant_emotion']
+        try:
+            data = data[0]
+            age = data['age']
+            gender = data['dominant_gender']
+            race = data['dominant_race']
+            emotion = data['dominant_emotion']
 
-        data = f'Результат анализа\n возраст: {age}\n пол: {gender}\n национальность: {race}\n эмоция: {emotion}'
-    except Exception as e:
-        print("Парсинг джейсона анализа")
-        return ('ANLS', None, 'ERR')
+            data = f'Результат анализа\n возраст: {age}\n пол: {gender}\n национальность: {race}\n эмоция: {emotion}'
+        except Exception as e:
+            print("Парсинг джейсона анализа")
+            return ('ANLS', None, 'ERR')
     
-    return ('ANLS', data, 'OK')
+        return ('ANLS', data, 'OK')
 
-def compare_photos(path1: str, path2: str) -> tuple:
-    '''
-    Метод верификации человека на двух фотографиях, отправленных пользователем.
-    input:
-        path: str - путь локального хранения фотографии, отправленной пользователем
-    output:
-        (type: str, data: str or None, status: str)
-        data - строка с ответом и дополнительной информацией в необходимом формате, которую получит пользователь от бота.
-    '''
-    try:
-        data = verify(img1_path=path1, img2_path=path2, detector_backend=BACKEND)
-    except Exception as e:
-        print("Работада метода верификации")
-        return ('CMPR', None, 'ERR')
+    def compare_photos(self, path1: str, path2: str) -> tuple:
+        '''
+        Метод верификации человека на двух фотографиях, отправленных пользователем.
+        input:
+            path: str - путь локального хранения фотографии, отправленной пользователем
+        output:
+            (type: str, data: str or None, status: str)
+            data - строка с ответом и дополнительной информацией в необходимом формате, которую получит пользователь от бота.
+        '''
+        try:
+            data = verify(img1_path=path1, img2_path=path2, detector_backend=self.__BACKEND)
+        except Exception as e:
+            print("Работада метода верификации")
+            return ('CMPR', None, 'ERR')
 
-    try:
-        verified = 'распознан ✅' if data['verified'] == True else 'разные люди ⛔'
-        distance = data['distance']
-        treshold_to_verify = data['threshold']
-        model = data['model']
-        similarity_metric = data['similarity_metric']
+        try:
+            verified = 'распознан ✅' if data['verified'] == True else 'разные люди ⛔'
+            distance = data['distance']
+            treshold_to_verify = data['threshold']
+            model = data['model']
+            similarity_metric = data['similarity_metric']
 
-        data = f'Результат верификации\n схожесть: {verified}\n расстрояние: {distance}\n порог: {treshold_to_verify}\n модель: {model}\n метрика схожести: {similarity_metric}'
-    except Exception as e:
-        print("Парсинг джейсона анализа")
-        return ('CMPR', None, 'ERR')
+            data = f'Результат верификации\n схожесть: {verified}\n расстрояние: {distance}\n порог: {treshold_to_verify}\n модель: {model}\n метрика схожести: {similarity_metric}'
+        except Exception as e:
+            print("Парсинг джейсона анализа")
+            return ('CMPR', None, 'ERR')
     
-    return ('CMPR', data, 'OK')
+        return ('CMPR', data, 'OK')
 
-def add_photo(path: str) -> tuple:
-    '''
-    Метод добавления фотографии в общую базу данных.
-    input:
-        path: str - путь локального хранения фотографии, отправленной пользователем
-    output:
-        (type: str, data: str or None, status: str)
-        data - строка с ответом о добавлении фотографии, которую получит пользователь от бота.
-    '''
-    global database_images
-    list_of_paths = database_images.path.to_list()
+    def add_photo(self, path: str) -> tuple:
+        '''
+        Метод добавления фотографии в общую базу данных.
+        input:
+            path: str - путь локального хранения фотографии, отправленной пользователем
+        output:
+            (type: str, data: str or None, status: str)
+            data - строка с ответом о добавлении фотографии, которую получит пользователь от бота.
+        '''
+        list_of_paths = self.__db.path.to_list()
 
-    if path.split("/")[1] in list_of_paths:
-        print("Фотография уже есть в БД")
-        data = 'Фотография не была добавлена, так как она уже есть в БД!'
-        return ('ADD', data, 'OK')
+        if path.split("/")[1] in list_of_paths:
+            print("Фотография уже есть в БД")
+            data = 'Фотография не была добавлена, так как она уже есть в БД!'
+            return ('ADD', data, 'OK')
 
-    try:
-        embedding_objs = represent(model_name=MODEL_NAME, 
-                      detector_backend=BACKEND, 
+        try:
+            embedding_objs = represent(model_name=self.__MODEL_NAME, 
+                      detector_backend=self.__BACKEND, 
                       img_path=path)
-    except Exception as e:
-        print("Получение эмбединга")
-        return ('ADD', None, 'ERR: Получение эмбединга')
+        except Exception as e:
+            print("Получение эмбединга")
+            return ('ADD', None, 'ERR: Получение эмбединга')
+
+        try:
+            messageTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            TimeStamp = str(messageTime)
+
+            result_post_photo = self.__service.post_photo(from_local_path=path, to_url=path)
+            
+            if result_post_photo == None:
+                return ('ADD', None, 'ERR')
+        except Exception as e:
+            print("Отправка в БД")
+            return ('ADD', None, 'ERR: Отправка в БД')
+
+        try:
+            dict = {
+                'path': path.split("/")[1], 
+                'vector': [embedding_objs[0]["embedding"]], 
+                'dateTime': TimeStamp, 
+                'url': path.split("/")[1]
+            }
+            new_image = pd.DataFrame({k: v for k, v in dict.items()})
+            new_image.to_csv("new.csv")
+
+            new_images = pd.read_csv("new.csv")
+            database_img = pd.read_csv("database.csv")
+            update_dataframe = pd.concat([database_img, new_images], ignore_index = True)
+            update_dataframe.to_csv("database.csv")
+        except Exception as e:
+            print("Добавление в dataset")
+            return ('ADD', None, 'ERR: Добавление в dataset')
     
-    print(path)
-    try:
-        messageTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        TimeStamp = str(messageTime)
+        try:
+            self.__db = self.__prepare_data("database.csv")
+        except Exception as e:
+            print("Обновление доступа к БД")
+            data = 'Фотография не была добавлена\nЧто-то пошло не так.. 😔'
+            return ('ADD', data, 'OK')
 
-        result_post_photo = post_photo(local_path=path, url=path)
-        if result_post_photo == None:
-           return ('ADD', None, 'ERR')
-    except Exception as e:
-        print("Отправка в БД")
-        return ('ADD', None, 'ERR: Отправка в БД')
-
-    try:
-        dict = {
-          'path': path.split("/")[1], 
-          'vector': [embedding_objs[0]["embedding"]], 
-          'dateTime': TimeStamp, 
-          'url': path.split("/")[1]
-        }
-        new_image = pd.DataFrame({k: v for k, v in dict.items()})
-        new_image.to_csv("new.csv")
-
-        new_images = pd.read_csv("new.csv")
-        database_img = pd.read_csv("database.csv")
-        update_dataframe = pd.concat([database_img, new_images], ignore_index = True)
-        update_dataframe.to_csv("database.csv")
-    except Exception as e:
-        print("Добавление в dataset")
-        return ('ADD', None, 'ERR: Добавление в dataset')
-    
-    try:
-        database_images = prepare_data("database.csv")
-    except Exception as e:
-        print("Обновление доступа к БД")
-        data = 'Фотография не была добавлена\nЧто-то пошло не так.. 😔'
+        data = 'Фотография добавлена в общую базу всех фотографий, спасибо! 🗃️'
         return ('ADD', data, 'OK')
-
-    data = 'Фотография добавлена в общую базу всех фотографий, спасибо! 🗃️'
-    return ('ADD', data, 'OK')
 
